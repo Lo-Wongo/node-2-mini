e81213361575873937161ccc0952341ddd25e6c4
lo-wongo <lmr.moses@gmail.com>
2018-01-25 14:47:53 -0700

    Solutions

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
+// import { read } from 'fs';
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const massive = require('massive');
+require('dotenv').config()
+const controller = require('./controller');
 
 const app = express();
+
+massive(
+    process.env.CONNECTION_STRING 
+  ).then(dbInstance => {
+    app.set( 'db', dbInstance);
+
+    // dbInstance.new_planes()
+    // .then(planes => console.log(planes) )
+    // .catch( err => console.log(err));
+
+    // dbInstance.get_planes()
+    // .then( planes => console.log(planes) )
+    // .catch( err => console.log(err));
+
+  });
+  
+
 app.use( bodyParser.json() );
 app.use( cors() );
 
-const port = process.env.PORT || 3000
+app.get('/api/planes/', controller.getPlanes);
+
+const port = process.env.PORT || 3030
 app.listen( port , () => { console.log(`Server listening on port ${port}`); } );
 
